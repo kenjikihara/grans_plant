0aa096c0c04df9c0a8e1191deb41d9a983587b27
Kenji Kihara <kenjikihara1220@gmail.com>
2018-08-07 00:26:08 +0000

    Post index/new/create 作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   
   def new
+    if params[:back]
+      @post = Post.new(post_params)
+      # @post.image.retrieve_from_cache! params[:cache][:image]
+    else
+      @post = Post.new
+    end
   end
   
   def show
   end
   
   def create
-    
+    Post.create(post_params)
+    redirect_to new_post_path
   end
   
   def update
@@ -22,8 +30,6 @@ class PostsController < ApplicationController
   private
   
   def post_params
-    
+    params.require(:post).permit(:grans_name, :grans_image, :plants_name, :plants_image, :transfer_complete, :image_cache)
   end
-  
-  
 end
