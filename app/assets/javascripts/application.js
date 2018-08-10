83994696feb887b48f633640601a1f5392e399ea
Kenji Kihara <kenjikihara1220@gmail.com>
2018-08-10 04:28:59 +0000

    bootstrap 修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min
+//= require jquery.js
+//= require bootstrap.min
 //= require activestorage
 //= require turbolinks
 //= require_tree .
