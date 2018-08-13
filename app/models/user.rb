c5f3e8bf143621463c9a950054b258a364dc4b7d
Kenji Kihara <kenjikihara1220@gmail.com>
2018-08-13 03:09:15 +0000

    deviseで許可するモジュールの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: %i(google)
+         
+  def self.create_unique_string
+    SecureRandom.uuid
+  end       
 end
